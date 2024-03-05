afbaa8aa8bfba84b372cde762ab59c94542de328
Martin Kiss <kiss.martin1111@gmail.com>
2024-03-05 20:29:12 +0100

    gameUtils refactor

--- utils/gameUtils.ts
+++ utils/gameUtils.ts
@@ -1,47 +1,5 @@
-import {Card, CardType,} from "../types/gameTypes";
-
-const cardTypes: CardType[] = [
-    {
-        id: 1,
-        image: "/images/cards/angular.png",
-    },
-    {
-        id: 2,
-        image: "/images/cards/d3.png",
-    },
-    {
-        id: 3,
-        image: "/images/cards/jenkins.png",
-    },
-    {
-        id: 4,
-        image: "/images/cards/postcss.png",
-    },
-    {
-        id: 5,
-        image: "/images/cards/react.png",
-    },
-    {
-        id: 6,
-        image: "/images/cards/redux.png",
-    },
-    {
-        id: 7,
-        image: "/images/cards/sass.png",
-    },
-    {
-        id: 8,
-        image: "/images/cards/splendex.png",
-    },
-    {
-        id: 9,
-        image: "/images/cards/ts.png",
-    },
-    {
-        id: 10,
-        image: "/images/cards/webpack.png",
-    },
-];
+import {Card} from "../types/gameTypes";
+import {cardTypes} from "../constants/game.const";
 
 export function createDeckSizesArray(start = 3, end = 10): number[] {
     const result: number[] = [];
