d73ea8cc4902b111658011dd0de83f65be620cc3
Martin Kiss <kiss.martin1111@gmail.com>
2024-03-05 11:15:56 +0100

    Header base design

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -1,8 +1,8 @@
 import Head from 'next/head'
 import styles from './layout.module.css'
-import utilStyles from '../../styles/utils.module.css'
 import Link from 'next/link'
 import React from "react";
+import Image from 'next/image'
 
 export const siteTitle = 'Card matching game';
 
@@ -19,16 +19,25 @@ export default function Layout({children, home}: LayoutProps) {
                 <meta name="og:title" content={siteTitle}/>
             </Head>
             <header className={styles.header}>
-                <>
-                    <h1 className={utilStyles.heading2Xl}>Base</h1>
-                </>
+                <div className={styles.leftSection}>
+                    <Image
+                        width={65}
+                        height={65}
+                        className={styles.headerLogo}
+                        src="/images/splendex.png"
+                        alt="logo"
+                    />
+                    <h1 className={styles.headerTitle}>splendex</h1>
+                </div>
             </header>
             <main>{children}</main>
-            {!home && (
-                <div className={styles.backToHome}>
-                    <Link href="/">Back</Link>
-                </div>
-            )}
+            <footer>
+                {!home && (
+                    <div className={styles.backToHome}>
+                        <Link href="/">Back</Link>
+                    </div>
+                )}
+            </footer>
         </div>
     )
 }
